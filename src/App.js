c6fafae6aefd88c15aeabd2919299696a20bbb20
Mike England <mike.england@webdevstudios.com>
2020-04-27 19:55:22 -0400

    Update JS Object structure to include multiples

--- src/App.js
+++ src/App.js
@@ -2,21 +2,41 @@ import React from 'react';
 import Layout from './layout/Layout';
 import All from './views/All';
 
+const postsDemo = {
+  post1: {
+    date: '12345',
+    title: 'Example Post 1',
+    image: {
+      imageURL: "#",
+      imageAlt: "Image Alt Value",
+    }
+  },
+  post2: {
+    date: '12345',
+    title: 'Example Post 1',
+    image: {
+      imageURL: "#",
+      imageAlt: "Image Alt Value",
+    }
+  },
+  post3: {
+    date: '12345',
+    title: 'Example Post 1',
+    image: {
+      imageURL: "#",
+      imageAlt: "Image Alt Value",
+    }
+  },
+}
+
+console.log( 'postsDemo', postsDemo );
+
 function App() {
   return (
     <Layout>
       <p>Content will go here...</p>
       <All
-        timelinePosts={ {
-          post: {
-            date: '12345',
-            title: 'Example Post 1',
-            image: {
-              imageURL: "#",
-              imageAlt: "Image Alt Value",
-            }
-          }
-        } }
+        timelinePosts={ postsDemo }
       />
     </Layout>
   );
