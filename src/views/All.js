3387f6ccf2192ee74b64f9c7f208b4396db797a8
Mike England <mike.england@webdevstudios.com>
2020-04-27 20:23:54 -0400

    Update All component
    - Abstracted Object.entries to variable.
    - Replaced list items with TimelinePost component
    - Destructured data Object for Props

--- src/views/All.js
+++ src/views/All.js
@@ -3,20 +3,38 @@ import TimelinePost from '../components/TimelinePost';
 
 const All = ( { timelinePosts } ) => {
 
+	// Convert Data Object to Array.
 	const posts = Object.entries( timelinePosts );
-
-	console.log( posts );
 
 	return (
 		<main>
-			{
-				Object.entries( timelinePosts ).map( ( data, key ) => (
-						<li key={ key }>
-							{ data.title }
-						</li>
-					)
-				)
-			}
+			{ posts.map( ( data, key ) => {
+				// Main object wrapper.
+				const postId = data[0];
+
+				// Props.
+				const {
+					category,
+					date,
+					image: {
+						imageAlt,
+						imageURL,
+					},
+					title,
+				} = data[1];
+
+				return(
+					<TimelinePost
+						category={ category }
+						date={ date }
+						imageAlt={ imageAlt }
+						imageURL={ imageURL }
+						key={ key }
+						postId={ postId }
+						title={ title }
+					/>
+				);
+			} ) }
 		</main>
 	);
 };
