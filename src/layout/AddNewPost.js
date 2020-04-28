873a01925d1d4daf528f98387c9c2e3f82f4554f
Mike England <mike.england@webdevstudios.com>
2020-04-27 21:50:07 -0400

    Wrap form sections in divs
    - Wrap in divs to fix field visual breaks

--- src/layout/AddNewPost.js
+++ src/layout/AddNewPost.js
@@ -14,34 +14,40 @@ const AddNewPost = () => {
 	return(
 		<form>
 			<h1>Add New Post</h1>
-			<label htmlFor="title">Title (3 to 60 characters):</label>
-			<input
-				id="title"
-				maxLength="60"
-				minLength="3"
-				name="title"
-				onChange={ (e) => (setFormTitle( e.target.value )) }
-				type="text"
-				value={ formTitle }
-			/>
-			<label htmlFor="category">Timeline Category</label>
-			<input
-				id="category"
-				maxLength="20"
-				minLength="3"
-				name="category"
-				onChange={ (e) => (setFormCategory( e.target.value )) }
-				type="text"
-				value={ formCategory }
-			/>
-			<label htmlFor="date">Timeline Post Date</label>
-			<input
-				id="date"
-				name="date"
-				onChange={ (e) => (setFormDate( e.target.value )) }
-				type="date"
-				value={ formDate }
-			/>
+			<div>
+				<label htmlFor="title">Title (3 to 60 characters):</label>
+				<input
+					id="title"
+					maxLength="60"
+					minLength="3"
+					name="title"
+					onChange={ (e) => (setFormTitle( e.target.value )) }
+					type="text"
+					value={ formTitle }
+				/>
+			</div>
+			<div>
+				<label htmlFor="category">Timeline Category</label>
+				<input
+					id="category"
+					maxLength="20"
+					minLength="3"
+					name="category"
+					onChange={ (e) => (setFormCategory( e.target.value )) }
+					type="text"
+					value={ formCategory }
+				/>
+			</div>
+			<div>
+				<label htmlFor="date">Timeline Post Date</label>
+				<input
+					id="date"
+					name="date"
+					onChange={ (e) => (setFormDate( e.target.value )) }
+					type="date"
+					value={ formDate }
+				/>
+			</div>
 			<button type="submit" onClick={ saveNewPost }>Submit</button>
 		</form>
 	);
