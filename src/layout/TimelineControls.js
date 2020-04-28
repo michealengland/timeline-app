df860ccb5cf7fac22620738579ac4e4805f8d88e
Mike England <mike.england@webdevstudios.com>
2020-04-27 21:20:24 -0400

    Add Button trigger
    Setup button onClick function toggle display AddNewForm

--- src/layout/TimelineControls.js
+++ src/layout/TimelineControls.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Controls from '../components/Controls';
+import AddNewForm from './AddNewForm';
 
-const TimelineControls = () => (
-	<Controls>
-		<button>Add New</button>
-	</Controls>
-);
+const TimelineControls = () => {
+	const [isNewPostActive, setNewPost] = useState(false);
+
+	const addNew = () => {
+		console.log( 'New Post is active', isNewPostActive );
+		if ( isNewPostActive === false ) {
+			setNewPost( true );
+		} else {
+			setNewPost( false );
+		}
+	};
+
+	return (
+		<Controls>
+			<button onClick={ addNew }>Add New</button>
+			{ isNewPostActive ? <AddNewForm /> : '' }
+		</Controls>
+	);
+};
 
 export default TimelineControls;
