5043115024095660b31017b5e4fa2a2ac94add98
Mike England <mike.england@webdevstudios.com>
2020-04-28 20:49:31 -0400

    Created basic login form

---
+++ src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleLogin = ( e ) => {
+		e.preventDefault();
+		console.log(email, password);
+	}
+
+	const emailOnchange = ( e ) => {
+		setEmail( e.target.value );
+	};
+
+	const passwordOnchange = ( e ) => {
+		setPassword( e.target.value );
+	};
+
+	return (
+		<form className="container"
+			name="login"
+			onSubmit={ handleLogin }
+		>
+			<p>
+				<label htmlFor="email">Email:</label>
+				<input
+					type="email"
+					onChange={ emailOnchange }
+				/>
+			</p>
+			<p>
+				<label htmlFor="password">Password:</label>
+				<input
+					type="password"
+					onChange={ passwordOnchange }
+				/>
+			</p>
+			<div>
+				<button
+					type="submit"
+					disabled={ ! email && ! password }
+				>
+					Login
+				</button>
+			</div>
+		</form>
+	);
+};
+
+export default Login;
