0074b239f4f28db9382ca5c9d34ef5e15e0f2c97
Mike England <mike.england@webdevstudios.com>
2020-05-04 09:42:28 -0400

    Large refactor of ImageUpload
    - Remove all onChange functionality out of the child component to fix issues
    with parent needing to manage state.
    - Remove onClick function from component to AddNewPost. This way the function
    is executed on form submit.
    - Cleaned up the component.

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,69 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import firebase from '../firebase';
+import React from 'react';
 
-// { userID, postID }
-
-const ImageUpload = ( { onChange, parentState } ) => {
-	const userID = 'testUser';
-
-	// Define storage.
-	const storage = firebase.storage();
-
-	// Set state.
-	// const [userID, setUserID] = useState('');
-	const [image, setImage] = useState();
-	const [url, setURL] = useState('');
-	const [localURL, setLocalURL] = useState('');
-	const [progress, setUploadProgress] = useState();
-
-	// useEffect(() => {
-	// 	console.log( 'CHILD parentState BEFORE:', parentState );
-	// 	onChange( 'kitty kitty 1234' );
-	// 	console.log( 'CHILD parentState AFTER:', parentState );
-	// }, [parentState, onChange]);
-
-	// Set Image on button click.
-	const uploadMedia = e => {
-		if ( e.target.files[0] ) {
-			setLocalURL( URL.createObjectURL( e.target.files[0] ) );
-			setImage( e.target.files[0] );
-		}
-	}
-
-	// On image submit.
-	const handleUpload = () => {
-		// Write Image to DB.
-		if ( ! userID ) {
-			return console.error( 'UID not provided.' );
-		}
-
-		const metadata = {
-			contentType: 'image/jpeg',
-		};
-
-		const uploadTask = storage.ref(`media/${ userID }/${ image.name }`).put(image, metadata);
-
-		uploadTask.on('state_changed',
-		(snapshot) => {
-			// Upload Progress.
-			const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-			setUploadProgress( progress );
-		},
-		(error) => {
-			// Upload Error.
-			console.error(error);
-		},
-		() => {
-			// Complete.
-			storage.ref(`media/${ userID }`).child(image.name).getDownloadURL().then(url => {
-				console.log( 'URL VALUE', url);
-				setURL( url );
-			})
-		});
-	}
-
+const ImageUpload = ( { onChange, progress, placeholderURL } ) => {
 	const uploadStyle = {
 		objectFit: 'cover',
+		height: 'auto',
 		maxWidth: '400px',
 	}
 
@@ -76,12 +16,11 @@ const ImageUpload = ( { onChange, parentState } ) => {
 			<input
 				name="image"
 				type="file"
-				onChange={ uploadMedia }
+				onChange={ onChange }
 			/>
-			<button onClick={ handleUpload }>Upload</button>
 			<br/>
 			{
-				<img style={ uploadStyle } src={ url || localURL || 'http://via.placeholder.com/400x300' } alt="Uploaded images" height="300" width="400"/>
+				<img style={ uploadStyle } src={ placeholderURL || 'http://via.placeholder.com/400x300' } alt="Uploaded images" height="300" width="400"/>
 			}
 		</div>
 	);
