ae4c1b39224fb862e45d93a128be0dde67c6ee18
Mike England <mike.england@webdevstudios.com>
2020-05-04 09:44:24 -0400

    Large refactor to include ImageUpload
    - Refactored form
    - Updated conditionals
    - Fix state handling on isNewTimeline
    - Setup write functions in onSubmit handler
    - Add states from ImageUpload
    - Create onChange to pass into ImageUpload which fixes issues with infinite
    loops

--- src/layout/AddNewPost.js
+++ src/layout/AddNewPost.js
@@ -1,39 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from '../firebase';
+import ImageUpload from '../components/ImageUpload';
+import { writeNewPost, writeNewTimeline } from '../utilities/write';
 
-/**
- * Generate a new post with unique ID.
- *
- * @param {*} timeline
- * @param {*} date
- * @param {*} title
- * @param {*} imageUrl
- */
-const writeNewPost = ( timeline, date, title, imageUrl ) => {
-	firebase.database().ref('timelines/').push( {
-		lineName: timeline,
-		lineSlug: timeline,
-		date: date,
-		title: title,
-		imageUrl : imageUrl
-	} );
-}
+const AddNewPost = ( { userID } ) => {
+	// Placeholder Data.
+	const getUserTimelines = () => ['example 1', 'example 2', 'example 3'];
+	const newPostID = '1234567';
 
-const AddNewPost = () => {
-	const [formTitle, setFormTitle] = useState('');
-	const [formCategory, setFormCategory] = useState('');
-	const [formImgURL, setFormImgURL] = useState('');
-	const [formDate, setFormDate] = useState('');
+	// Define Firebase Storage.
+	const storage = firebase.storage();
 
+	// Set Form States.
+	const [date, setDate] = useState('');
+	const [image, setImage] = useState('this is ');
+	const [isNewTimeline, setIsNewTimeline] = useState( true );
+	const [placeholderURL, setPlaceholderURL] = useState('');
+	const [progress, setUploadProgress] = useState(0);
+	const [timelineNew, setNewTimeline] = useState('');
+	const [title, setTitle] = useState('');
+	const [url, setURL] = useState('');
+
+	// Image upload event handler.
+	const uploadMedia = e => {
+		if( e.target.files[0] ) {
+			setPlaceholderURL( URL.createObjectURL( e.target.files[0] ) );
+			setImage( e.target.files[0] );
+		}
+	}
+
+	// Image upload write event.
+	const writeMediaToStorage = ( image, progress, userID, url ) => {
+		// Write Image to DB.
+		if ( ! userID ) {
+			return console.error( 'UID not provided.' );
+		}
+
+		const metadata = {
+			contentType: 'image/jpeg',
+		};
+
+		const uploadTask = storage.ref(`media/${ userID }/${ image.name }`).put(image, metadata);
+
+		uploadTask.on('state_changed',
+		(snapshot) => {
+			// Upload Progress.
+			const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+			setUploadProgress( progress );
+		},
+		(error) => {
+			// Upload Error.
+			console.error(error);
+		},
+		() => {
+			console.log( 'witeMEDIA STARTED' );
+			// Complete.
+			storage.ref(`media/${ userID }`).child(image.name).getDownloadURL().then( mediaURL => {
+				console.log( 'URL VALUE', mediaURL);
+				setURL( mediaURL );
+			})
+		});
+	}
+
+	// Toggle isNewTimeline on checkbox click.
+	const toggleNewTimeline = () => {
+		if ( isNewTimeline === true ) {
+			setIsNewTimeline( false );
+		} else {
+			setIsNewTimeline( true );
+		}
+	};
+
+	/**
+	 * On Submit writeNewPost data.
+	 *
+	 * @param {*} e event.
+	 */
 	const saveNewPost = ( e ) => {
 		e.preventDefault();
-		setFormCategory( formCategory );
-		setFormDate( formDate );
-		setFormTitle( formTitle );
-		setFormImgURL( formImgURL );
+		setNewTimeline( timelineNew );
+		setDate( date );
+		setTitle( title );
 
-		// write to the DB.
-		writeNewPost( formCategory, formDate, formTitle, formImgURL );
+		// Write image to the storage DB.
+		writeMediaToStorage( image, progress, userID, url );
+
+		// Write post to the DB.
+		writeNewPost( userID, date, url, title );
+
+		// Write / Edit timeline to the DB.
+		if ( isNewTimeline ) {
+			console.log( 'NEW TIMELINE CREATED' );
+			writeNewTimeline( userID, timelineNew, newPostID );
+		}
 	};
 
 	return(
@@ -46,41 +105,64 @@ const AddNewPost = () => {
 					maxLength="60"
 					minLength="3"
 					name="title"
-					onChange={ (e) => (setFormTitle( e.target.value )) }
+					onChange={ (e) => { setTitle( e.target.value ) } }
 					type="text"
-					value={ formTitle }
+					value={ title }
 				/>
 			</div>
+
 			<div>
-				<label htmlFor="category">Timeline</label>
+				<label htmlFor="new-timeline">Where would you like to add this post?</label><br />
 				<input
-					id="category"
-					maxLength="20"
-					minLength="3"
-					name="category"
-					onChange={ (e) => (setFormCategory( e.target.value )) }
-					type="text"
-					value={ formCategory }
+					checked={ isNewTimeline }
+					id="existing-timeline"
+					name="timeline-assign"
+					onChange={ toggleNewTimeline }
+					type="checkbox"
 				/>
+				<label htmlFor="existing-timeline">Use New Timeline</label><br />
 			</div>
+
 			<div>
-				<label htmlFor="image-url">Image URL</label>
-				<input
-					id="image-url"
-					name="image-url"
-					onChange={ (e) => (setFormImgURL( e.target.value )) }
-					type="url"
-					value={ formImgURL }
-				/>
+				{ isNewTimeline === true ?
+					<>
+						<label htmlFor="category">New Timeline Name</label>
+						<input
+							id="category"
+							maxLength="20"
+							minLength="3"
+							name="category"
+							onChange={ (e) => { setNewTimeline( e.target.value ) } }
+							type="text"
+							value={ timelineNew }
+						/>
+					</>
+				:
+					<>
+						<label htmlFor="timeline-select">Select a Timeline</label>
+						<select id="timeline-select">
+							{
+								getUserTimelines().map( ( timeline, key ) => (
+									<option key={ key } value={ timeline }>{ timeline }</option>
+								) )
+							}
+						</select>
+					</>
+				}
 			</div>
+			<ImageUpload
+				placeholderURL={ placeholderURL }
+				progress={ progress }
+				onChange={ uploadMedia }
+			/>
 			<div>
-				<label htmlFor="date">Date</label>
+				<label htmlFor="date">Post Display Date</label>
 				<input
 					id="date"
 					name="date"
-					onChange={ (e) => (setFormDate( e.target.value )) }
+					onChange={ (e) => (setDate( e.target.value ) ) }
 					type="date"
-					value={ formDate }
+					value={ date }
 				/>
 			</div>
 			<button type="submit" onClick={ saveNewPost }>Submit</button>
