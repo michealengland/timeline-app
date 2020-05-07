4479f90811096122e1649af0f9fc52f7aa6e5d9b
Mike England <mike.england@webdevstudios.com>
2020-05-07 18:01:48 -0400

    Modify Welcome component

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Login from '../components/Login';
 
-const Welcome = () => (
+const Welcome = () => {
+
+	const style= {
+		margin: '0 auto',
+		maxWidth: '400px',
+		padding: '20px',
+		border: '1px solid #eee',
+	}
+
+	return(
 		<>
-			<h1>Welcome to Timeline App!</h1>
-			<p>Get started...</p>
+			<div
+				style={ style }
+			>
+			<h2>Welcome to Timeline App!</h2>
+			<p>Sign in or create an account...</p>
+			<Login />
+			</div>
 		</>
-);
+	)
+};
 
 export default Welcome;
