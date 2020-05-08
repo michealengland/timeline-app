5829c22c3b782360f51ceec86ee5f016bb3d42e2
Mike England <mike.england@webdevstudios.com>
2020-05-08 00:49:25 -0400

    WIP Write Media Function Updates

--- src/utilities/write.js
+++ src/utilities/write.js
@@ -112,51 +112,73 @@ async function writeMediaToStorage( file, uid ) {
 	// Upload file and metadata to the object 'images/mountains.jpg'
 	const uploadTask = storageRef.child(`media/${ uid }/${ file.name }`).put(file, metadata);
 
-	return new Promise( ( resolve ) => {
-		// Listen for state changes, errors, and completion of the upload.
-		uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-		function(snapshot) {
-			// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-			var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-			console.log('Upload is ' + progress + '% done');
-			// eslint-disable-next-line default-case
-			switch (snapshot.state) {
-			case firebase.storage.TaskState.PAUSED: // or 'paused'
-				console.log('Upload is paused');
-				break;
-			case firebase.storage.TaskState.RUNNING: // or 'running'
-				console.log('Upload is running');
-				break;
-			}
-		}, (error) => {
+	uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+	(snapshot) => {
+		// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+		var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+		console.log('Upload is ' + progress + '% done');
+		// eslint-disable-next-line default-case
+		switch (snapshot.state) {
+		case firebase.storage.TaskState.PAUSED: // or 'paused'
+			console.log('Upload is paused');
+			break;
+		case firebase.storage.TaskState.RUNNING: // or 'running'
+			console.log('Upload is running');
+			break;
+		}
+	}, (error) => {
 		// A full list of error codes is available at
 		// https://firebase.google.com/docs/storage/web/handle-errors
 		// eslint-disable-next-line default-case
 		switch (error.code) {
 			case 'storage/unauthorized':
-			// User doesn't have permission to access the object
 			break;
 
 			case 'storage/canceled':
-			// User canceled the upload
 			break;
 
 			case 'storage/unknown':
-			// Unknown error occurred, inspect error.serverResponse
 			break;
 		}
-		}, () => {
-			// Upload completed successfully, now we can get the download URL
-			uploadTask.snapshot.ref.getDownloadURL().then( (downloadURL) => {
-				console.log('File available at', downloadURL);
-				return resolve( downloadURL );
-			});
+	}, () => {
+		// Upload completed successfully, now we can get the download URL
+		uploadTask.snapshot.ref.getDownloadURL().then( (downloadURL) => {
+			console.log('File available at', downloadURL);
+			return downloadURL;
 		});
 	});
 }
 
-const uploadMediaToStorage = async ( file, uid ) => {
-	const newMediaURL = await writeMediaToStorage( file, uid );
+
+async function UPLOAD_IMAGES( file, uid ) {
+
+	// Assign a timestamp to randomize file name.
+	const date = Date.now();
+
+	try {
+
+		const storageRef = firebase.storage().ref();
+
+		// Create the file metadata
+		const metadata = {
+			contentType: "image/jpeg"
+		};
+
+		const fileRef = storageRef.child(`media/${ uid }/${ date + '-' + file.name }`);
+
+		const uploadTaskSnapshot = await fileRef.put(file, metadata);
+
+		const downloadURL = await uploadTaskSnapshot.ref.getDownloadURL();
+
+		return downloadURL;
+	} catch (error) {
+		console.error( error );
+	}
+}
+
+async function uploadMediaToStorage( file, uid ) {
+	const newMediaURL = await UPLOAD_IMAGES( file, uid );
+	console.log( 'BEFORE RETURN', newMediaURL );
 
 	return newMediaURL;
 }
