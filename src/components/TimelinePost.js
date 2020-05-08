3594c1281a66f8b96bba9ad6bd300db298d756e7
Mike England <mike.england@webdevstudios.com>
2020-05-08 01:41:00 -0400

    Update Timeline to display categories based on query

--- src/components/TimelinePost.js
+++ src/components/TimelinePost.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import firebase from '../firebase';
 
 const TimelinePost = ( { date, imageURL, slug, title, timeline } ) => {
+	const [currentTimeline, setCurrentTimeline ] = useState('');
+
 	const style = {
 		padding: '.2em',
 	};
+
+	useEffect(() => {
+		const getTimelineData = async () => {
+			const findTimeline = firebase.database().ref( 'timelines/' + timeline );
+			findTimeline.on('value', function(snapshot) {
+
+				// Set currentTimeline state.
+				if ( currentTimeline === '' ) {
+					setCurrentTimeline( snapshot.val() );
+				}
+			});
+		}
+
+		getTimelineData();
+	}, [currentTimeline, timeline] );
 
 	return (
 		<article>
 			<div style={ style }>
 				<h1><Link to={`/post/${ slug }`}>{ title }</Link></h1>
-				<span><Link to={`/timeline/${ timeline }`}>{ timeline }</Link></span>
+				<span><Link to={`/timeline/${ currentTimeline.slug }`}>{ currentTimeline.label }</Link></span>
 				<p>{ date }</p>
 			</div>
 			{
