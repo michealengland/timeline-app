146285fe7542cb17fe74922c28da69bc3ec7feb9
Mike England <mike.england@webdevstudios.com>
2020-05-08 00:48:02 -0400

    Update timeline post

--- src/components/TimelinePost.js
+++ src/components/TimelinePost.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const TimelinePost = ( { date, imageURL, slug, title, timeline } ) => (
-	<article>
-		<h1><Link to={`/post/${ slug }`}>{ title }</Link></h1>
-		<span><Link to={`/timeline/${ timeline }`}>{ timeline }</Link></span>
-		<p>{ date }</p>
-		{
-			imageURL && <img src={ imageURL } />
-		}
-	</article>
-);
+const TimelinePost = ( { date, imageURL, slug, title, timeline } ) => {
+	const style = {
+		padding: '.2em',
+	};
+
+	return (
+		<article>
+			<div style={ style }>
+				<h1><Link to={`/post/${ slug }`}>{ title }</Link></h1>
+				<span><Link to={`/timeline/${ timeline }`}>{ timeline }</Link></span>
+				<p>{ date }</p>
+			</div>
+			{
+				imageURL && <img src={ imageURL } alt={ title } />
+			}
+		</article>
+	);
+};
 
 export default TimelinePost;
