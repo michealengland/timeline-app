e60abc00c0ed8153fd8b024f8d3cbdf3a8eb9531
Mike England <mike.england@webdevstudios.com>
2020-05-16 12:18:10 -0400

    Add All link to timeline control

--- src/layout/TimelineControls.js
+++ src/layout/TimelineControls.js
@@ -47,6 +47,7 @@ const TimelineControls = ( { onChange, theme, uid } ) => {
 
 	return (
 		<Controls>
+			<Link style={ style } to={`/all`}>All</Link>
 			<Link style={ style } to={`/add-new-post`}>New Post</Link>
 			<button style={ style } onClick={ sortByDate }>{ sortDatesLabel }</button>
 			<button style={ style } onClick={ toggleTheme }>{ currentTheme }</button>
