684a452262dbd0b1893e11a16bf4b57c7080039d
Mike England <mike.england@webdevstudios.com>
2020-05-16 11:42:45 -0400

    Update Welcome to display new post form.

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Login from '../components/Login';
 import AuthForm from '../layout/AuthForm';
+import AddNewPost from '../layout/AddNewPost';
 
-const Welcome = ( { onLogin } ) => (
+const Welcome = ( { uid } ) => (
 	<AuthForm>
 		<h2>Welcome to Timeline App!</h2>
 		<p>Sign in or create an account...</p>
-		<Login onLogin={ onLogin } />
+		<AddNewPost uid={ uid } />
 	</AuthForm>
 );
 
