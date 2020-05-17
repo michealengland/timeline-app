78a3bf4a58bbc7e1ceba0a5238e9d6826e7db34d
Mike England <mike.england@webdevstudios.com>
2020-05-17 13:12:45 -0400

    Setup Material DateTimePicker
    - Create Material DateTimePicker based on example from site
    - Create parent state update function
    - Install necessary packaages

---
+++ src/components/MaterialDatePicker.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import DateFnsUtils from '@date-io/date-fns'; // choose your lib
+import {
+  DatePicker,
+  TimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+
+const MaterialDatePicker = ( { onUpdate } ) => {
+  const [selectedDate, handleDateChange] = useState(new Date());
+
+  // Update state in parent component.
+  useEffect(() => {
+    onUpdate( selectedDate );
+  }, [onUpdate, selectedDate]);
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <DatePicker name="date" value={selectedDate} onChange={handleDateChange} />
+      <TimePicker name="date" value={selectedDate} onChange={handleDateChange} />
+    </MuiPickersUtilsProvider>
+  );
+}
+
+export default MaterialDatePicker;
