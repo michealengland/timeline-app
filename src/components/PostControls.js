af78c0759c551fb14454bf7bda04af4ca6880800
Mike England <mike.england@webdevstudios.com>
2020-12-05 13:18:31 -0500

    Replace html button with EditPostButton

--- src/components/PostControls.js
+++ src/components/PostControls.js
@@ -1,21 +1,18 @@
 import React, { useState } from 'react';
 import { deletePost, deleteMediaFromStorage, deletePostFromTimeline } from '../utilities/delete';
 import { Redirect } from 'react-router-dom';
+import EditPostButton from './EditPostButton'
 
 const PostControls = ( props ) => {
 	const {
 		editPost,
 		id,
 		imageURL,
+		isEditing,
 		timeline,
 	} = props;
 
 	const [redirect, setRedirect] = useState( false );
-
-	const onEditPostClick = () => {
-		console.log( 'EDIT POST CLICKED' );
-		editPost(true)
-	}
 
 	const onDeleteClick = () => {
 		console.log( 'DELETE POST CLICKED' );
@@ -31,7 +28,7 @@ const PostControls = ( props ) => {
 
 	return (
 		<nav className="post-controls">
-			<button onClick={ onEditPostClick }>Edit</button>
+			<EditPostButton isEditing={isEditing} editToggle={editPost} />
 			<button onClick={ onDeleteClick }>Delete</button>
 			{ redirect === true && <Redirect to="/" /> }
 		</nav>
