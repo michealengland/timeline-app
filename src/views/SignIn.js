435236f9f42aa71772bf29c666e66cfedd01fb14
Mike England <mike.england@webdevstudios.com>
2021-04-16 12:19:38 -0400

    Move Login to molecules

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from '../components/Login';
+import Login from '../components/molecules/Login';
 import AuthForm from '../layout/AuthForm';
 
 const SignIn = ( { onLogin } ) => (
