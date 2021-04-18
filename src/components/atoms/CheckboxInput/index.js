372cb7d3abc9675df926e624538cce5a57d50a4c
Mike England <mike.england@webdevstudios.com>
2021-04-18 03:16:17 -0400

    Create CheckboxInput

---
+++ src/components/atoms/CheckboxInput/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// @TODO Add error handling
+export default function CheckboxInput(props) {
+  const {checked, id, label, onChange} = props
+
+  return (
+    <>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        checked={checked}
+        id={id}
+        name={id}
+        onChange={onChange}
+        type="checkbox"
+        {...props}
+      />
+    </>
+  )
+}
+
+CheckboxInput.propTypes = {
+  checked: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+}
+
+CheckboxInput.defaultProps = {
+  checked: false,
+}
+
