0cbfeb9662ed84514d08417c6e032d04670ac252
Mike England <mike.england@webdevstudios.com>
2021-04-18 02:54:33 -0400

    Setup TextInput

---
+++ src/components/atoms/TextInput/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function TextInput(props) {
+  const {id, label, onBlur, onChange, value} = props
+
+  return (
+    <>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        id={id}
+        name={id}
+        onBlur={onBlur}
+        onChange={onChange}
+        type="text"
+        value={value}
+        {...props}
+      />
+    </>
+  )
+}
+
+TextInput.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+}
