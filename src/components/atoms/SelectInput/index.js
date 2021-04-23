cf368f5732a15b4db576f82a08447f1a3256deea
Mike England <mike.england@webdevstudios.com>
2021-04-23 09:15:00 -0400

    Abstract select to component

---
+++ src/components/atoms/SelectInput/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// @TODO Add error handling
+export default function SelectInput(props) {
+  const {id, label, onBlur, onChange, value, options} = props
+
+  return (
+    <div className="select-input">
+      {label && <label htmlFor={id}>{label}</label>}
+      <select
+        defaultValue={options[0].value}
+        id={id}
+        name={id}
+        onBlur={onBlur}
+        onChange={onChange}
+        value={value}
+        {...props}
+      >
+        {options.length > 0 &&
+          options.map(({option, value}) => (
+            <option key={value} value={value}>
+              {option}
+            </option>
+          ))}
+      </select>
+    </div>
+  )
+}
+
+SelectInput.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      option: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  value: PropTypes.string,
+}
