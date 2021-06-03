7c9c9a44a43365ac2bcebbdf5458a2012e246e7c
Mike England <mike.england@webdevstudios.com>
2021-06-02 20:33:02 -0400

    Update config with latest config
    The firebase API key is intended for public use.
    
    Issue #28
    
    See https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public/37484053#37484053

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,16 @@ import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/storage'
 
-const config = {
-  authDomain: 'timeline-app-bab5f.firebaseapp.com',
-  databaseURL: 'https://timeline-app-bab5f.firebaseio.com',
-  projectId: 'timeline-app-bab5f',
-  storageBucket: 'timeline-app-bab5f.appspot.com',
-  messagingSenderId: '485647181936',
-  appId: '1:485647181936:web:2df837530f50e5595decb5',
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyCSEIhAp2di9BDh0itMjwEzCTX6WlChY_E",
+  authDomain: "timeline-app-bab5f.firebaseapp.com",
+  databaseURL: "https://timeline-app-bab5f.firebaseio.com",
+  projectId: "timeline-app-bab5f",
+  storageBucket: "timeline-app-bab5f.appspot.com",
+  messagingSenderId: "485647181936",
+  appId: "1:485647181936:web:fbc6a57e9fb734485decb5"
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(firebaseConfig)
 
 export default firebase
