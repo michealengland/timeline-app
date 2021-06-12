30b99d2cf46848e45dc069bf302efd65434fe1a0
Mike England <mike.england@webdevstudios.com>
2021-06-12 15:54:56 -0400

    Setup placeholder updatePost on edit button

---
+++ src/components/molecules/PostControls/index.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import {
+  deletePost,
+  deleteMediaFromStorage,
+  deletePostFromTimeline,
+} from '../../../utilities/delete'
+import {updatePost} from '../../../utilities/edit'
+import {Redirect} from 'react-router-dom'
+
+const PostControls = props => {
+  const {id, imageURL, timeline} = props
+
+  console.log('PROPS', props)
+
+  const [redirect, setRedirect] = useState(false)
+
+  const editPost = () => {
+    updatePost(id, {
+      title: 'Palm Trees',
+      slug: 'palm-trees',
+    })
+  }
+
+  const onDeleteClick = () => {
+    console.log('DELETE POST CLICKED')
+    deletePost(id, timeline)
+    deletePostFromTimeline(id, timeline)
+
+    if (imageURL) {
+      deleteMediaFromStorage(imageURL)
+    }
+
+    setRedirect(true)
+  }
+
+  return (
+    <nav className="post-controls">
+      <button onClick={editPost}>Edit</button>
+      <button onClick={onDeleteClick}>Delete</button>
+      {redirect === true && <Redirect to="/" />}
+    </nav>
+  )
+}
+
+PostControls.propTypes = {
+  id: PropTypes.string,
+  imageURL: PropTypes.string,
+  timeline: PropTypes.object,
+}
+
+export default PostControls
