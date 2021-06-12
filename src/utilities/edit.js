33790bdbc4f28b9004a5606616c955ba948f7ce3
Mike England <mike.england@webdevstudios.com>
2021-06-12 15:48:56 -0400

    Abstract data processing into function
    This function will be very helpful in generating updates for Firebase.
    By taking an Object of path such as `posts/postKey/` this function will
    generate a new Object with keys for each value change ex:
    `posts/postKey/propertyFromObj`.
    
    Note: This function should be moved to a more centralized location.

--- src/utilities/edit.js
+++ src/utilities/edit.js
@@ -1,24 +1,40 @@
 import firebase from '../firebase'
 
 /**
+ * Process data Object into path based Object for Firebase.
+ *
+ * @param {string} path    Path of value to update in Firebase.
+ * @param {Object} dataObj Data Object to create Firebase update Object from.
+ * @return {Object}
+ */
+function generateFirebaseUpdateObject(path, dataObj) {
+  const valuesToUpdate = {}
+  const dataKeys = Object.keys(dataObj)
+
+  for (let i = 0; i < dataKeys.length; i++) {
+    valuesToUpdate[`${path}/${dataKeys[i]}`] = dataObj[dataKeys[i]]
+  }
+
+  return valuesToUpdate
+}
+
+/**
  * Update properties on `posts/postKey`.
+ *
+ * Note: Empty Objects, do not trigger updates in firebase.
  *
  * @param {string} postKey        Key of post to updated.
  * @param {Object} updatedPostObj Object of updated post values to send to Firebase.
  */
-function updatePost(postKey, updatedPostObj) {
+function updatePost(postKey, updatedPostObj = {}) {
   if (!postKey) {
     return
   }
 
-  const valuesToUpdate = {}
-  const keys = Object.keys(updatedPostObj)
-
-  for (let i = 0; i < keys.length; i++) {
-    valuesToUpdate[`/posts/${postKey}/${keys[i]}`] = updatedPostObj[keys[i]]
-  }
-
-  firebase.database().ref().update(valuesToUpdate)
+  firebase
+    .database()
+    .ref()
+    .update(generateFirebaseUpdateObject(`/posts/${postKey}`, updatedPostObj))
 }
 
 export {updatePost}
