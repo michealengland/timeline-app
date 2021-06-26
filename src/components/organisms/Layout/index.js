50650dc6805a0a9d354feaa5ed8b808962461acc
Mike England <mike.england@webdevstudios.com>
2021-06-26 09:25:29 -0400

    Chore Layout named function and directory

--- src/components/organisms/Layout/index.js
+++ src/components/organisms/Layout/index.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
-import Header from './Header'
-import Footer from './Footer'
-import Loading from '../atoms/Loading'
-import TimelineControls from './TimelineControls'
+import Header from '../Header'
+import Footer from '../Footer'
+import Loading from '../../atoms/Loading'
+import TimelineControls from '../TimelineControls'
 
-const Layout = ({changePostDirection, children, hasPosts, onLogout, uid}) => {
+export default function Layout({changePostDirection, children, hasPosts, onLogout, uid}) {
   const lightMode = {
     backgroundColor: '#fffef9',
     color: '#232329',
@@ -62,5 +62,3 @@ Layout.propTypes = {
 Layout.defaultProps = {
   hasPosts: false,
 }
-
-export default Layout
