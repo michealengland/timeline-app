f1ca15253edfb265ce76998bcb9687011715160b
Mike England <mike.england@webdevstudios.com>
2021-06-26 09:21:30 -0400

    Chore move Footer to directory

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import projectConfig from '../../projectConfig'
+import projectConfig from '../../../projectConfig'
 
 export default function Footer() {
   return <footer>{`${projectConfig.name} 2020`}</footer>
