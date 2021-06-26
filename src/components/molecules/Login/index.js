574af043811a8d3311557745c0dd5615b6a553bb
Mike England <mike.england@webdevstudios.com>
2021-06-26 09:08:09 -0400

    Chore migrate Login to directory

--- src/components/molecules/Login/index.js
+++ src/components/molecules/Login/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
-import firebase from '../../firebase'
+import firebase from '../../../firebase'
 import {Link} from 'react-router-dom'
 
 // Log in user.
