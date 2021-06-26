6306381d157c6cbccdfcf0f4ac73e5ec29a384bc
Mike England <mike.england@webdevstudios.com>
2021-06-26 09:17:44 -0400

    Chore Footer named function

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import projectConfig from '../../projectConfig'
 
-const Footer = () => <footer>{`${projectConfig.name} 2020`}</footer>
+export default function Footer() {
+  return <footer>{`${projectConfig.name} 2020`}</footer>
+}
 
 Footer.propTypes = {
   copyRightText: PropTypes.string,
 }
-
-export default Footer
